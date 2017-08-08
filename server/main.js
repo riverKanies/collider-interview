f5075423ce408ea78cfc3211127b667e3864296e
River Kanies <river.kanies@gmail.com>
2017-08-08 12:50:12 -0400

    init stl viewer/upload in meteor app

---
+++ server/main.js
@@ -0,0 +1,11 @@
+import { Meteor } from 'meteor/meteor';
+
+Meteor.startup(() => {
+  // code to run on server at startup
+});
+
+function mainTest () {
+  return 'mainTest'
+}
+
+export default mainTest
---
+++ server/main.test.js
@@ -0,0 +1,10 @@
+import { chai } from 'meteor/practicalmeteor:chai';
+import mainTest from './main'
+
+describe('my module', function () {
+  it('does something that should be tested', function () {
+    // This code will be executed by the test driver when the app is started
+    // in the correct mode
+    chai.assert.equal(mainTest(), 'mainTest')
+  })
+})
