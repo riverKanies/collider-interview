9bdbfbf4860419edbeb763b316dcc8b9aba1b1f9
River Kanies <river.kanies@gmail.com>
2017-08-08 13:24:57 -0400

    list stl urls from server

--- client/main.js
+++ client/main.js
@@ -2,21 +2,29 @@ import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
+import { Stlfiles } from '../imports/api/stlfiles.js';
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
+Template.list.onCreated(function helloOnCreated() {
+  this.autorun(()=>{
+    this.subscribe('stlfiles')
+  })
 });
 
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-});
-
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
-});
+Template.list.helpers({
+  files() {
+    return Stlfiles.find({})//[{url:'1'}, {url:'2'}, {url:'3'}]
+  }
+})
+//
+// Template.hello.helpers({
+//   counter() {
+//     return Template.instance().counter.get();
+//   },
+// });
+//
+// Template.hello.events({
+//   'click button'(event, instance) {
+//     // increment the counter when button is clicked
+//     instance.counter.set(instance.counter.get() + 1);
+//   },
+// });
---
+++ imports/api/stlfiles.js
@@ -0,0 +1,3 @@
+import { Mongo } from 'meteor/mongo';
+
+export const Stlfiles = new Mongo.Collection('stlfiles');
